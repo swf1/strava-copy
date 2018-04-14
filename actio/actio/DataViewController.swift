1dbaeb2458d2baaa02e6506fefdfabab7f0f0181
AleitaT <traina@oregonstate.edu>
2018-04-14 10:17:47 -0700

    initial commit

---
+++ actio/actio/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  actio
+//
+//  Created by Alieta Train on 4/14/18.
+//  Copyright © 2018 corvus group. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+  @IBOutlet weak var dataLabel: UILabel!
+  var dataObject: String = ""
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.dataLabel!.text = dataObject
+  }
+
+
+}
+
