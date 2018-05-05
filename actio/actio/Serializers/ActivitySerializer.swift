6eb9ce3e043ffda2283e9f439f3956a975846962
Tyler McGinnis <tylermcginnis@gmail.com>
2018-05-05 15:59:54 -0700

    Add ActivitySerializer and ActivityStorageAdapter, update docblocks.

--- actio/actio/Serializers/ActivitySerializer.swift
+++ actio/actio/Serializers/ActivitySerializer.swift
@@ -10,13 +10,3 @@
 //
 
 import Foundation
-
-class Athlete: NSObject {
-    var uid: String
-    var email: String
-
-    init(uid: String, email: String) {
-        self.uid = uid
-        self.email = email
-    }
-}
